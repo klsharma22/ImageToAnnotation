4c5d48d811a6e35d28467ac94717ea820ade3e5f
klsharma22 <30960037+klsharma22@users.noreply.github.com>
2023-10-14 23:51:15 +0530

    Mouse Movement Captured

--- ImageToAnnotation/app.py
+++ ImageToAnnotation/app.py
@@ -2,24 +2,36 @@ import tkinter as tk
 from tkinter import ttk
 import logging
 
-class App(tk.Tk):
-    def __init__(self):
-            super().__init__()
+class App:
+      def __init__(self, root):
+            self.root = root
             logging.basicConfig(filename= 'app.log', filemode= 'w', format=' %(name)s - %(levelname)s - %(message)s')
-            self.geometry("300x300")
-            xmin_label, xmax_label = ttk.Label(self, text= "xmin"), ttk.Label(self, text= "xmax")
-            ymin_label, ymax_label = ttk.Label(self, text= "ymin"), ttk.Label(self, text= "ymax")
+            self.root.geometry("1000x1000")
+            self.xmin_label, self.xmax_label = ttk.Label(self.root, text= "xmin"), ttk.Label(self.root, text= "xmax")
+            self.ymin_label, self.ymax_label = ttk.Label(self.root, text= "ymin"), ttk.Label(self.root, text= "ymax")
 
-            if xmin_label and xmax_label and ymin_label and ymax_label:
+            if self.xmin_label and self.xmax_label and self.ymin_label and self.ymax_label:
                   logging.info("Labels have been initiated successfully.")
             else:
                   logging.error("Labels have not been initiated successfully.")
 
-            xmin_label.place(relx=0.2, rely=0.1)
-            xmax_label.place(relx=0.66, rely=0.1)
-            ymin_label.place(relx=0.2, rely= 0.4)
-            ymax_label.place(relx=0.66, rely=0.4)
+            self.xmin_label.place(relx=0.05, rely=0.025)
+            self.xmax_label.place(relx=0.25, rely=0.025)
+            self.ymin_label.place(relx=0.05, rely=0.125)
+            self.ymax_label.place(relx=0.25, rely=0.125)
+
+            frame = tk.Frame(self.root, width= 500, height= 500)
+            frame.place(relx=0.05, rely=0.2)
+
+            frame.bind('<Motion>', self.mouse_coordinate)
+
+      def mouse_coordinate(self, event):
+            self.xmin_label.config(text= f"{event.x}")
+            self.ymin_label.config(text= f"{event.y}")
 
 
-app = App()
-app.mainloop()
+
+if __name__ == '__main__':
+      root = tk.Tk()
+      app = App(root)
+      root.mainloop()
